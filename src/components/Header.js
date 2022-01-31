609a05991754e7d652ec76cd772d1e9f47269b53
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-31 16:50:05 -0600

    Create components and header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <nav>
+      <p>Image navbar </p>
+    </nav>
+    <nav>
+        <p>Links nav</p>
+    </nav>
+  </header>
+);
+
+export default Header;
---
+++ src/components/Missions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Missions = () => (
+    <h1>Missions Section</h1>
+);
+
+export default Missions;
