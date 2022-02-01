adfff64f5849ea22f0ebebba21dc6d883ed9d982
ivaan1992 <ivaan.lg.92@gmail.com>
2022-02-01 10:18:21 -0600

    Add rockets folder

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,30 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-<<<<<<< HEAD
-import thunk from 'redux-thunk';
-import rocketsReducer from './rockets/rockets';
-import missionsReducer from './missions/missions';
-
-const reducer = combineReducers({
-  rocketsReducer,
-  missionsReducer,
-});
-
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk),
-);
-
-export default store;
-=======
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionsReducer from './missions/missions';
+import rocketsReducer from './rockets/rockets';
 
 const rootReducer = combineReducers({
   missionsReducer,
+  rocketsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 export default store;
->>>>>>> dev
