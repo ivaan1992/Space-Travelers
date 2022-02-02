3e3066bd29863785557cf72cb57652147ce31ffa
Oliver Coimbra <olivercoimbra@gmail.com>
2022-02-02 16:58:03 -0400

    Add store to profile feature

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import missionsReducer from './missions/missions';
 import rocketsReducer from './rockets/rockets';
+import missionsReducer from './missions/missions';
 
 const rootReducer = combineReducers({
-  missionsReducer,
   rocketsReducer,
+  missionsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
